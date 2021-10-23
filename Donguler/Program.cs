f2c2a80bfe1b476eaa3e14cc551edf83ef7abdea
43ism <43ism@DESKTOP-QULI0KK>
2021-10-23 20:00:05 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç için Temel Kurs";
+            string kurs3 = "Jawa";
+            string kurs4 = "Python";
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+
+            // array - Diziler
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı",
+                "Programlamaya Başlangıç için Temel Kurs",
+                "Jawa","Python","c#"};
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("for bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("Sayfa sonu");
+        }
+    }
+}
---
+++ odev/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            urun urun1 = new urun();
+            urun1.UrunAdi = "Mouse";
+            urun1.UrunOzellik = " Kablosuz";
+
+            urun urun2 = new urun();
+            urun2.UrunAdi = "Laptop";
+            urun2.UrunOzellik = "Taşınabilir.";
+
+            urun urun3 = new urun();
+            urun3.UrunAdi = "Telefon";
+            urun3.UrunOzellik = "Konuşabilir";
+
+            urun[] urunler = new urun[] { urun1, urun2, urun3 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.UrunAdi+" "+urun.UrunOzellik);
+            }
+
+        }
+    }
+    class urun
+    {
+        public string UrunAdi { get; set; }
+        public int UrunFiyatı { get; set; }
+        public string UrunOzellik { get; set; }
+    }
+}
